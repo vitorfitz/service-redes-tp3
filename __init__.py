66f216e0a47f37906f643e1d616246b65d9710a3
guilherme13c <guilhermesccaporali@protonmail.com>
2024-08-02 12:15:46 -0300

    feat: add web

--- __init__.py
+++ __init__.py
@@ -1,4 +1,6 @@
-from flask import Flask, request, jsonify
+# __init__.py
+
+from flask import Flask, request, jsonify, send_from_directory
 from database import load_game_data
 from api import api_blueprint
 from flask_swagger_ui import get_swaggerui_blueprint
@@ -9,13 +11,15 @@ def create_app():
     load_game_data("scores.jsonl")
     app.register_blueprint(api_blueprint)
     SWAGGER_URL = '/swagger'
-
     API_URL = '/static/swagger.yaml'
-
     swaggerui_blueprint = get_swaggerui_blueprint(
         SWAGGER_URL,
         API_URL,
     )
-
     app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
+
+    @app.route('/')
+    def serve_index():
+        return send_from_directory('static', 'index.html')
+
     return app
